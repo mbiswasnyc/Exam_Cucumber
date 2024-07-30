b16bcf70866951c04cf50eaa3fa84646966ddd57
mbiswas <mbiswasnyc@gmail.com>
2024-07-30 00:23:44 -0400

    1st commit

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", monochrome = true, dryRun = false,
-        tags = "@PIM or @Login or @Dashboard",
+        tags = "@PIM or @Dashboard",
         plugin = {"html:target/cucumber.html", "json:target/cucumber.json",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:target/cucumber.extent"})
 
@@ -13,9 +13,9 @@ import org.testng.annotations.DataProvider;
 public class TestNGTestRunner extends AbstractTestNGCucumberTests
 {
     // run all test parallel
-    @Override
+    /*@Override
     @DataProvider(parallel = true)
     public Object[][] scenarios(){
         return super.scenarios();
-    }
+    }*/
 }
